1a48a30b0bc46da5d6d3768fb84068614b8df61e
jo <ljonas@riseup.net>
2021-04-16 15:28:36 +0200

    Add comment on FirstWeekContainsDate type

--- src/types.ts
+++ src/types.ts
@@ -29,6 +29,13 @@ export interface LocaleOptions {
   locale?: Locale
 }
 
+/**
+ * FirstWeekContainsDate is used to determine which week is the first of the year, based on what day the 1 January is in that week.
+ *
+ * The day in that week can only be 1 (Monday) or 4 (Thursday).
+ *
+ * Please see https://en.wikipedia.org/wiki/Week#Week_numbering for more information.
+ */
 export type FirstWeekContainsDate = 1 | 4
 
 export interface FirstWeekContainsDateOptions {
